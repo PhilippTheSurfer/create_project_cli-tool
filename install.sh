0d2bcddf0995594e29ffa3aa26180eaea90d9ccb
philippthesurfer <philipp.lehmann@gruppe.ai>
2024-11-21 04:17:16 +0100

    add an install script to run as command

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Ensure the script is run as a superuser
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root. Use sudo to execute it."
+   exit 1
+fi
+
+echo "Start installation process..."
+chmod +x project.py
+
+cp project.py /usr/local/bin/project
+
+echo "Updating system and installing required packages... (python3, pip, venv, click)"
+apt update
+apt install -y python3 python3-pip python3-venv python3-typer
+
+echo "All dependencies are installed successfully!"
+echo "The CLI tool 'project' is now globally accessible."
+echo "For more information type 'project --help'."
