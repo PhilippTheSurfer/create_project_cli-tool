c776796994efbce51b3acebd5c1ff534c8e54515
philippthesurfer <philipp.lehmann@gruppe.ai>
2024-11-24 01:15:14 +0100

    intorduce an install script for the project command

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ chmod +x project.py
 
 cp project.py /usr/local/bin/project
 
-echo "Updating system and installing required packages... (python3, pip, venv, click)"
-apt update
-apt install -y python3 python3-pip python3-venv python3-typer
+#echo "Updating system and installing required packages... (python3, pip, venv, typer, typing-extension)"
+#apt update
+#apt install -y python3 python3-pip python3-venv python3-typer python3-typing-extensions
 
 echo "All dependencies are installed successfully!"
 echo "The CLI tool 'project' is now globally accessible."
